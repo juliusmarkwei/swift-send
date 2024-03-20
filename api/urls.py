e88542cf3184b209d745a7519ce6d4ce0605701a
Julius Markwei <juliusmarkwei2000@gmail.com>
2024-03-20 23:44:05 +0000

    included msg endpoints

--- api/urls.py
+++ api/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('contact', views.ContactView.as_view(), name='contacts-view'),
     path('contact/<int:contactID>', views.ContactView.as_view(), name='contacts-view'),
     path('send-sms', views.SendSMSView.as_view(), name='send-sms-view'),
+    path('message', views.MessageView.as_view(), name='message-view'),
+    path('message/<int:messageID>', views.MessageView.as_view(), name='message-view'),
 ]
--- api/views.py
+++ api/views.py
@@ -1,6 +1,6 @@
 from django.contrib.auth import get_user_model
 from contacts.models import Contact
-from messages_center.models import Message
+from messages_center.models import Message, ContactMessage
 from msg_templates.models import Template
 from rest_framework.views import APIView
 from rest_framework.response import Response
@@ -8,6 +8,8 @@ from rest_framework import status
 from .serializers import ContactSerializer, MessageSerializer, TemplateSerializer
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from .send_sms import send_sms
+from .utils import clean_contacts
+
 
 User = get_user_model()
 
@@ -62,21 +64,155 @@ class SendSMSView(APIView):
     
     def post(self, request):
         data = request.POST
-        recipients = data['to']
-        if ',' in recipients:
-            recipients_list = [recipient.strip() for recipient in recipients.split(',')]
-        elif ' ' in recipients:
-            recipients_list = [recipient.strip() for recipient in recipients.split()]
-        elif ';' in recipients:
-            recipients_list = [recipient.strip() for recipient in recipients.split(';')]
-        elif '\n' in recipients:
-            recipients_list = [recipient.strip() for recipient in recipients.split('\n')]
-        else:
-            recipients_list = [recipients.strip()]
-            
         message = data['message']
-        print(recipients_list, message)
+        recipients = data['to']
+        recipients_list = clean_contacts(recipients)
         
-        send_sms(message=message, to=recipients_list)
-        
+        try:
+            response = send_sms(message=message, to=recipients_list)
+        except Exception as e:
+            return Response({'message': f'Error: {e}'}, status=status.HTTP_400_BAD_REQUEST)
         return Response({'message': 'Message delivered!'}, status=status.HTTP_204_NO_CONTENT)
+    
+    
+class MessageView(APIView):
+    permission_classes = [IsAuthenticated]
+    
+    def get(self, request, messageID=None):
+        user = request.user
+        if messageID:
+            try:
+                message = Message.objects.get(pk=messageID, created_by=user)
+                serializer = MessageSerializer(message)
+                return Response(serializer.data, status=status.HTTP_200_OK)
+            except Message.DoesNotExist:
+                return Response({'message': 'Message not found'}, status=status.HTTP_404_NOT_FOUND)
+        else:
+            messages = Message.objects.filter(created_by=user)
+            if not messages:
+                return Response({'message': 'No messages found'}, status=status.HTTP_404_NOT_FOUND)
+            serializer = MessageSerializer(messages, many=True)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    def post(self, request):
+        try:
+            user = request.user
+            request_data = request.data.copy()
+            request_data['created_by'] = User.objects.get(username=user).id
+            
+            serializer = MessageSerializer(data=request_data)
+           
+            if serializer.is_valid():
+                message = serializer.save()
+              
+                contacts = request_data.get('contacts', [])
+                if contacts:
+                    recipient_lists = clean_contacts(contacts)
+                    try:
+                        print(message)
+                        response = send_sms(message=message, to=recipient_lists)
+                        print('yayyyyyyyyyy')
+                        contacts_objects = Contact.objects.filter(pk__in=contacts)
+                        for contact in contacts_objects:
+                            ContactMessage.objects.create(contact=contact, message=message)
+                    except Exception as e:
+                        return Response({'message': f'Error: {e}'}, status=status.HTTP_400_BAD_REQUEST)
+                    return Response({'message': 'Message delivered!'}, status=status.HTTP_204_NO_CONTENT)
+                    
+                
+                return Response(serializer.data, status=status.HTTP_201_CREATED)
+            else:
+                return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        except User.DoesNotExist:
+            return Response({'message': 'User does not exist'}, status=status.HTTP_400_BAD_REQUEST)
+        except Exception as e:
+            return Response({'message': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+    
+    def put(self, request, messageID):
+        user = request.user
+        try:
+            user_messages = Message.objects.filter(created_by=user)
+            message = user_messages.get(pk=messageID)
+        except Message.DoesNotExist:
+            return Response({'message': 'Message not found'}, status=status.HTTP_404_NOT_FOUND)
+        
+        serializer = MessageSerializer(message, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def delete(self, request, messageID):
+        try:
+            user = request.user
+            user_message = Message.objects.filter(created_by=user)
+            message = user_message.get(pk=messageID)
+            message.delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        except Message.DoesNotExist:
+            return Response({'message': 'Message not found'}, status=status.HTTP_404_NOT_FOUND)
+        except Exception as e:
+            return Response({'message': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        
+
+class TemplateView(APIView):
+    permission_classes = [IsAuthenticated]
+    
+    def get(self, request, templateID=None):
+        user = request.user
+        if templateID:
+            try:
+                template = Template.objects.get(pk=templateID, created_by=user)
+                serializer = TemplateSerializer(template)
+                return Response(serializer.data, status=status.HTTP_200_OK)
+            except Template.DoesNotExist:
+                return Response({'message': 'Template not found'}, status=status.HTTP_404_NOT_FOUND)
+        else:
+            templates = Template.objects.filter(created_by=user)
+            if not templates:
+                return Response({'message': 'No templates found'}, status=status.HTTP_404_NOT_FOUND)
+            serializer = TemplateSerializer(templates, many=True)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    def post(self, request):
+        try:
+            user = request.user
+            request_data = request.data.copy()
+            request_data['created_by'] = User.objects.get(username=user).id
+            serializer = TemplateSerializer(data=request_data)
+            
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data, status=status.HTTP_201_CREATED)
+            else:
+                return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        except User.DoesNotExist:
+            return Response({'message': 'User does not exist'}, status=status.HTTP_400_BAD_REQUEST)
+        except Exception as e:
+            return Response({'message': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+    
+    def put(self, request, templateID):
+        user = request.user
+        try:
+            user_templates = Template.objects.filter(created_by=user)
+            template = user_templates.get(pk=templateID)
+        except Template.DoesNotExist:
+            return Response({'message': 'Template not found'}, status=status.HTTP_404_NOT_FOUND)
+        
+        serializer = TemplateSerializer(template, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def delete(self, request, templateID):
+        try:
+            user = request.user
+            user_template = Template.objects.filter(created_by=user)
+            template = user_template.get(pk=templateID)
+            template.delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        except Template.DoesNotExist:
+            return Response({'message': 'Template not found'}, status=status.HTTP_404_NOT_FOUND)
+        except Exception as e:
+            return Response({'message': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
