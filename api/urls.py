e9f6a6c663ef3f60098fd3b91682d44fcc872f92
Julius Markwei <juliusmarkwei2000@gmail.com>
2024-03-20 18:45:41 +0000

    added contacts & sms

--- api/urls.py
+++ api/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
+from . import views
 
 urlpatterns = [
-    
+    path('contact', views.ContactView.as_view(), name='contacts-view'),
+    path('contact/<int:contactID>', views.ContactView.as_view(), name='contacts-view'),
+    path('send-sms', views.SendSMSView.as_view(), name='send-sms-view'),
 ]
