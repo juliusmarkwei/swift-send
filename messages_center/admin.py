af488d9ce077ec554bd5cc719899ac9232698d34
Julius Markwei <juliusmarkwei2000@gmail.com>
2024-03-21 23:56:48 +0000

    removed message sender app

--- messages_center/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Message
-
-@admin.register(Message)
-class MessageAdmin(admin.ModelAdmin):
-    list_display = ('id', 'message_content', 'status', 'created_at', 'updated_at')
-    search_fields = ('message_content',)
-    ordering = ('-created_at',)
