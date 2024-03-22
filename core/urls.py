ee58d70c3ea90a86e3827a7aa34df72d2545fb29
Julius Markwei <juliusmarkwei2000@gmail.com>
2024-03-22 04:18:04 +0000

    added swagger doc emdpoint

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
-from api.utils import schema_view
+from utility.docs_info import schema_view
 from djoser import urls
 
 
@@ -9,5 +9,6 @@ urlpatterns = [
     path('auth/', include('djoser.urls')),
     path('auth/', include('djoser.urls.jwt')),
     path('api/', include('api.urls')),
-    path('', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
