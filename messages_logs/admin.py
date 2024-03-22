aef7385fed9f00b04c1ea565aa9b07d8dae7beb3
Julius Markwei <juliusmarkwei2000@gmail.com>
2024-03-22 04:19:12 +0000

    fixed db_name error

--- messages_logs/admin.py
+++ messages_logs/admin.py
@@ -1,14 +1,14 @@
 from django.contrib import admin
-from .models import MessageLog, ReceipientLog
+from .models import MessageLog, RecipientLog
 
 @admin.register(MessageLog)
 class MessageAdmin(admin.ModelAdmin):
-    list_display = ('id', 'content', 'status', 'sent_by', 'sent_at', 'status')
+    list_display = ('id', 'content', 'status', 'author', 'sent_at', 'status')
     search_fields = ('content',)
     ordering = ('-sent_at',)
     
 
-@admin.register(ReceipientLog)
+@admin.register(RecipientLog)
 class ReceipientAdmin(admin.ModelAdmin):
     list_display = ('id', 'contact', 'message')
     search_fields = ('contact',)
--- messages_logs/models.py
+++ messages_logs/models.py
@@ -7,28 +7,28 @@ User = get_user_model()
 
 class MessageLog(models.Model):   
     content = models.TextField(max_length=255)
-    sent_by = models.ForeignKey(User, on_delete=models.CASCADE, db_column='created_by')
+    author = models.ForeignKey(User, on_delete=models.PROTECT, db_column='authorId')
     sent_at = models.DateTimeField(auto_now_add=True)
     status = models.CharField(max_length=20)
     
     def __str__(self):
-        return self.id
+        return str(self.id)
     
     class Meta:
         verbose_name = 'Message Log'
         verbose_name_plural = 'Message Logs'
-        db_table = 'message log'
+        db_table = 'message_log'
         
-        
-class ReceipientLog(models.Model):
+    
+class RecipientLog(models.Model):   
     contact = models.ForeignKey(Contact, on_delete=models.CASCADE, db_column='receipient')
     message = models.ForeignKey(MessageLog, on_delete=models.CASCADE, db_column='message')
     
     def __str__(self):
-        return self.contact
+        return str(self.contact_id)
     
     class Meta:
-        verbose_name = 'Receipient Log'
-        verbose_name_plural = 'Receipient Logs'
-        db_table = 'receipient log'
+        verbose_name = 'Recipient Log'
+        verbose_name_plural = 'Recipient Logs'
+        db_table = 'recipient_log'
         
