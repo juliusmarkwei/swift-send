caffd961a7a04499dbbe6d10710da088e3ea4df8
Julius Markwei <juliusmarkwei2000@gmail.com>
2024-03-26 15:50:42 +0000

    increased auth tokens

--- core/settings.py
+++ core/settings.py
@@ -155,8 +155,8 @@ REST_FRAMEWORK = {
 
 SIMPLE_JWT = {
     'AUTH_HEADER_TYPES': ('JWT',),
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
-    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=10),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=20),
 }
 
 DJOSER = {
